bde847ab631cc20c3c830dd8cc4b1b7a059b7d96
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2022-10-05 10:39:47 -0400

    gossage

--- magix/action/loginAction.php
+++ magix/action/loginAction.php
@@ -9,6 +9,28 @@
 
         protected function executeAction() {
             
-            return [];
+            $hasConnectionError = false;
+            
+            if(!empty($_POST["username"])){
+                $data = [];
+                $data["username"] = $_POST["username"];
+                $data["password"] = $_POST["pwd"];
+                $_SESSION["user"] = $_POST["username"];
+                $result = parent::callAPI("signin", $data);
+
+                if ($result == "INVALID_USERNAME_PASSWORD") {
+                    $hasConnectionError = true;
+                }
+                else {
+                    var_dump($result);exit;
+                    $key = $result->key;
+                }
+
+            }
+            
+            return compact("hasConnectionError", "key");
+
+
+        
         }
     }
