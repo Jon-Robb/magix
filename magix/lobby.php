1cc49d96d195be09395f18fe8d0cb82b454db195
Jon-Robb <robinsonjonathan240817@gmail.com>
2022-10-12 18:35:44 -0400

    mise en place des boutons et du wall paper

--- magix/lobby.php
+++ magix/lobby.php
@@ -13,16 +13,30 @@
             <p> Bonjour Mech-Guerrier <?= $data["username"] ?> </p>
         </div>
         <div class="buttons-section">
-            <div class="button-section-firstRow">
-                <button class="btn-jouer"> Jouer </button>
-                <button class="btn-pratique"> Pratique </button>
+            <div class="button-section-firstCol">
+                <button class="btn-jouer"> Jouer solo </button>
+                <button class="btn-pratique"> Pratique solo </button>
+                <button class="btn-historique"> Historique </button>
+                <button class="btn-quitter"> Quitter </button>
+            </div>                   
+        </div>
+        <div class="chat-section">
+            <iframe style="width:700px;height:240px;" onload="applyStyles(this)" 
+                    src="https://magix.apps-de-cours.com/server/#/chat/<?= $data["key"] ?>">
+            </iframe>
+        </div>
+        <div class="button-section">
+            <div class="button-section-secondCol">
+                <button class="btn-coop"> Jouer Coop </button>
+                <input type="text" placeholder="Clé privée (optionnel)" name="input-coop-jouer">
+                <button class="btn-pratique-coop"> Pratique Coop</button>
+                <input type="text" placeholder="Clé privée (optionnel)" name="input-coop-pratique">
+                <button class="btn-observer"> Observer </button>
+                <input type="text" placeholder="Nom du joueur" name="input-nom-joueur">
             </div>
         </div>
     </div>
-    <iframe style="width:700px;height:240px;" onload="applyStyles(this)" 
-        src="https://magix.apps-de-cours.com/server/#/chat/<?= $data["key"] ?>">
-    </iframe>
-
+   
 
 <?php
     require_once("partial/footer.php");
