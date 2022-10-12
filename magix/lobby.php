fa998450abe45c8285bad0fb84915474e39f304f
Jon-Robb <robinsonjonathan240817@gmail.com>
2022-10-12 19:14:10 -0400

    travail sur le layout du lobby

--- magix/lobby.php
+++ magix/lobby.php
@@ -12,27 +12,30 @@
         <div class="user-greetings">
             <p> Bonjour Mech-Guerrier <?= $data["username"] ?> </p>
         </div>
-        <div class="buttons-section">
-            <div class="button-section-firstCol">
-                <button class="btn-jouer"> Jouer solo </button>
-                <button class="btn-pratique"> Pratique solo </button>
-                <button class="btn-historique"> Historique </button>
-                <button class="btn-quitter"> Quitter </button>
-            </div>                   
-        </div>
-        <div class="chat-section">
-            <iframe style="width:700px;height:240px;" onload="applyStyles(this)" 
-                    src="https://magix.apps-de-cours.com/server/#/chat/<?= $data["key"] ?>">
-            </iframe>
-        </div>
-        <div class="button-section">
-            <div class="button-section-secondCol">
-                <button class="btn-coop"> Jouer Coop </button>
-                <input type="text" placeholder="Clé privée (optionnel)" name="input-coop-jouer">
-                <button class="btn-pratique-coop"> Pratique Coop</button>
-                <input type="text" placeholder="Clé privée (optionnel)" name="input-coop-pratique">
-                <button class="btn-observer"> Observer </button>
-                <input type="text" placeholder="Nom du joueur" name="input-nom-joueur">
+        <div class="menu-section">
+            <div class="buttons-section">
+                <div class="button-section-firstCol">
+                    <button class="btn-jouer"> Jouer solo </button>
+                    <button class="btn-pratique"> Pratique solo </button>
+                    <button class="btn-historique"> Historique </button>
+                    <button class="btn-quitter"> Quitter </button>
+                </div>                   
+            </div>
+            <!-- style="width:700px;height:240px;" -->
+            <div class="chat-section">
+                <iframe  onload="applyStyles(this)" 
+                        src="https://magix.apps-de-cours.com/server/#/chat/<?= $data["key"] ?>">
+                </iframe>
+            </div>
+            <div class="button-section">
+                <div class="button-section-secondCol">
+                    <button class="btn-coop"> Jouer Coop </button>
+                    <input type="text" placeholder="Clé privée (optionnel)" name="input-coop-jouer">
+                    <button class="btn-pratique-coop"> Pratique Coop</button>
+                    <input type="text" placeholder="Clé privée (optionnel)" name="input-coop-pratique">
+                    <button class="btn-observer"> Observer </button>
+                    <input type="text" placeholder="Nom du joueur" name="input-nom-joueur">
+                </div>
             </div>
         </div>
     </div>
