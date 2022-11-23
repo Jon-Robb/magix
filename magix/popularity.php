41de32848022744f8a5cb124f3eb24f9e8790e3b
Jon-Robb <robinsonjonathan240817@gmail.com>
2022-11-23 11:43:12 -0500

    page statique pour les cartes les plus jouées. Doit peuplé à l'école

--- magix/popularity.php
+++ magix/popularity.php
@@ -1,7 +1,71 @@
 <?php
 require_once("action/PopularityAction.php");
+require_once("partial/header.php");
+?>
+
+<div class="popularity-body">
+
+    <div class="popularity-title">
+        <h1>
+            Cartes les plus populaires
+        </h1>
+    </div>
+    <div class="palmares">
+        <div class="palmares-titles palmares-line">
+            <h3>
+                ID
+            </h3>
+            <h3>
+                Cost
+            </h3>
+            <h3>
+                Hp
+            </h3>
+            <h3>
+                Attack
+            </h3>
+            <h3>
+                Mechanics
+            </h3>
+            <h3>
+                Times played
+            </h3>
+            <h3>
+                Ratio
+            </h3>
+        </div>
+        <div class="palmares-line">
+            <p>
+                28
+            </p>
+            <p>
+                4
+            </p>
+            <p>
+                8
+            </p>
+            <p>
+                3
+            </p>
+            <p>
+                DeathRattle : Summon a 7/7 minion with taunt
+            </p>
+            <p>
+                234
+            </p>
+            <p>
+                58.8%
+            </p>
+        </div>
+    </div>
+
+
+</div>
 
 
 
+
+
+<?php
 
 require_once("partial/footer.php");
